933f7db36ab37bc2b270e41b4a4e3c11c4cfcb4a
Angga Saputra <anggasaputra2st@gmail.com>
2025-02-05 10:23:32 +0800

    Update: change lorem ipsum

--- app/components/ViewHome.tsx
+++ app/components/ViewHome.tsx
@@ -51,21 +51,21 @@ export default function ViewHome() {
                 <FaMoneyBillWave />
               </div>
               <h3 className="font-semibold mb-1">Save</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+              <p>save the amount of money you have</p>
             </div>
             <div className="flex flex-col items-center min-w-full md:min-w-52 md:w-52 snap-center">
               <div className="bg-primary w-14 h-12 rounded flex justify-center items-center text-3xl mb-2">
                 <FaBell />
               </div>
               <h3 className="font-semibold mb-1">Reminder</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+              <p>Remind yourself of the amount of money you have</p>
             </div>
             <div className="flex flex-col items-center min-w-full md:min-w-52 md:w-52 snap-center">
               <div className="bg-primary w-14 h-12 rounded flex justify-center items-center text-3xl mb-2">
                 <FaPiggyBank />
               </div>
               <h3 className="font-semibold mb-1">Count</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+              <p>Calculate the amount of money you have</p>
             </div>
           </div>
         </div>
@@ -73,24 +73,24 @@ export default function ViewHome() {
           <Image src={'/images/holiday.png'} alt="holiday" width={423} height={414} className="w-[323px] h-[314px] md:w-[423px] md:h-[414px]" />
           <div className="flex flex-col justify-center">
             <div className="mb-7">
-              <h2 className="mb-2 font-semibold text-xl">Why you should save your money</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum eveniet iste sint quas quibusdam recusandae quidem tempora soluta, sunt nesciunt qui facilis nulla, modi quod esse dicta quam alias!</p>
+              <h2 className="mb-2 font-semibold text-xl">Why you should keep financial records</h2>
+              <p>By maintaining accurate records of your income, expenses, and savings, you can track where your money is going, identify spending patterns, and make informed decisions about budgeting, investing, or cutting unnecessary costs. This helps you stay on top of your financial health, avoid unnecessary debt, and achieve your financial goals more efficiently.</p>
             </div>
             <div>
               <h2 className="mb-2 font-semibold text-xl">Why you should save your money</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum eveniet iste sint quas quibusdam recusandae quidem tempora soluta, sunt nesciunt qui facilis nulla, modi quod esse dicta quam alias! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ut fugiat quisquam facilis distinctio, quibusdam perferendis natus velit odio similique doloribus aperiam fugit ea a sapiente. Labore architecto esse beatae.</p>
+              <p>You should save your money because it provides financial security and freedom. Having savings ensures that you are prepared for emergencies, unexpected expenses, or future goals like buying a house, starting a business, or retirement. It also reduces financial stress and gives you more options in life, allowing you to take advantage of opportunities without relying on debt.</p>
             </div>
           </div>
         </div>
         <div id="options" className="flex gap-8 flex-col md:flex-row">
           <div className="flex flex-col justify-center order-2 md:order-1">
             <div className="mb-5">
-              <h2 className="mb-2 font-semibold text-xl">Benefits when you sign up</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum eveniet iste sint quas quibusdam recusandae quidem tempora soluta, sunt nesciunt qui facilis nulla, modi quod esse dicta quam alias!</p>
+              <h2 className="mb-2 font-semibold text-xl">Let&apos;s start with a demo</h2>
+              <p>with a demo, you can see how the system works for free! In the demo, We implement a local storage system so your data will be safe because the data will be stored on the device you use!</p>
             </div>
             <div className="flex gap-2">
               <Link href="/sign-up" className="button-primary w-fit py-2 px-3 rounded flex items-center gap-2">Sign Up <FaRightToBracket /></Link>
-              <Link href="/trial" className="button-primary w-fit py-2 px-6 rounded flex items-center gap-2">Trial <FaHardDrive /></Link>
+              <Link href="/trial" className="button-primary w-fit py-2 px-6 rounded flex items-center gap-2">Demo <FaHardDrive /></Link>
             </div>
           </div>
           <Image src={'/images/note.png'} alt="note" width={410} height={410} className="order-1 w-[310px] h-[310px] md:order-2 md:w-[410px] md:h-[410px]" />
