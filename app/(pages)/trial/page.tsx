6437aeb772b196412e6ed1234f60398334097c12
Angga Saputra <anggasaputra2st@gmail.com>
2025-04-04 20:34:09 +0800

    Fix: Image in demo

--- app/(pages)/trial/page.tsx
+++ app/(pages)/trial/page.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 import ViewFooter from "@/app/components/ViewFooter";
-import ViewNavbar from "@/app/components/ViewNavbar"
+import ViewNavbar from "@/app/components/ViewNavbar";
 import { Data } from "@/app/types/types";
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -9,7 +9,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Trial = () => {
-  const [imageUrl, setImageUrl] = useState<string>('/images/count.png');
+  const [imageUrl, setImageUrl] = useState<string>("/images/count.png");
   const [data, setData] = useState<Data>({
     user: {
       name: "",
@@ -28,11 +28,11 @@ const Trial = () => {
         description: "",
         is_increase: false,
         date_now: new Date(),
-      }
+      },
     ],
     image: {
-      username: ""
-    }
+      username: "",
+    },
   });
   useEffect(() => {
     AOS.init({
@@ -48,12 +48,12 @@ const Trial = () => {
         console.log("Data tidak ditemukan di localStorage.");
       }
     }
-  }, [])
+  }, []);
   useEffect(() => {
     if (data.image.username.length != 0 && isValidUrl(data.image.username)) {
       setImageUrl(data.image.username);
     }
-  }, [data])
+  }, [data]);
   function isValidUrl(url: string) {
     try {
       new URL(url);
@@ -83,43 +83,104 @@ const Trial = () => {
       <div className="w-10/12 mx-auto relative flex flex-col gap-24">
         <ViewNavbar isLanding={false} savedData={data} setSavedData={setData} />
         <div className="m-auto mt-28 overflow-x-hidden">
-          <h1 data-aos="fade-left" className="bg-primary w-fit p-3 rounded font-medium text-4xl md:text-6xl">{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(data.saving.total_saving)}</h1>
-          <p data-aos="fade-right" className="mt-3 text-center">My Savings</p>
+          <h1
+            data-aos="fade-left"
+            className="bg-primary w-fit p-3 rounded font-medium text-4xl md:text-6xl"
+          >
+            {new Intl.NumberFormat("id-ID", {
+              style: "currency",
+              currency: "IDR",
+            }).format(data.saving.total_saving)}
+          </h1>
+          <p data-aos="fade-right" className="mt-3 text-center">
+            My Savings
+          </p>
         </div>
         <div className="flex flex-col gap-5 overflow-hidden">
           {/* --- */}
           <div className="flex w-full gap-5 flex-col md:flex-row order-2 md:order-1 overflow-hidden">
-            <Image data-aos="fade-right" src={imageUrl} alt="count" width={423} height={353} className="rounded w-80 h-80 object-cover" />
+            <Image
+              data-aos="fade-right"
+              src={imageUrl}
+              alt="count"
+              width={423}
+              height={353}
+              className="rounded w-full sm:w-80 h-80 object-cover"
+            />
             <div className="w-full flex flex-col gap-5">
-              <h2 data-aos="fade-left" className="w-full bg-primary rounded text-3xl font-semibold p-2 text-center">Details</h2>
-              <div data-aos="fade-up" className="h-full bg-[#121212] rounded p-3 text-center flex flex-col">
-                <h3 className="text-primary font-semibold mb-2">Total Money I Used</h3>
-                <p className="flex-grow flex justify-center items-center text-2xl md:text-5xl">{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(data.saving.total_money_used)}</p>
+              <h2
+                data-aos="fade-left"
+                className="w-full bg-primary rounded text-3xl font-semibold p-2 text-center"
+              >
+                Details
+              </h2>
+              <div
+                data-aos="fade-up"
+                className="h-full bg-[#121212] rounded p-3 text-center flex flex-col"
+              >
+                <h3 className="text-primary font-semibold mb-2">
+                  Total Money I Used
+                </h3>
+                <p className="flex-grow flex justify-center items-center text-2xl md:text-5xl">
+                  {new Intl.NumberFormat("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                  }).format(data.saving.total_money_used)}
+                </p>
               </div>
             </div>
           </div>
           {/* --- */}
           <div className="flex w-full gap-5 flex-col md:flex-row order-1 md:order-2 overflow-hidden">
             <div className="w-full flex flex-col gap-5">
-              <h2 data-aos="fade-left" className="w-full bg-primary rounded text-3xl font-semibold p-2 text-center">{data.user.name}</h2>
-              <div data-aos="fade-up" className="h-full bg-[#121212] rounded p-3">
+              <h2
+                data-aos="fade-left"
+                className="w-full bg-primary rounded text-3xl font-semibold p-2 text-center"
+              >
+                {data.user.name}
+              </h2>
+              <div
+                data-aos="fade-up"
+                className="h-full bg-[#121212] rounded p-3"
+              >
                 <p>{data.user.description}</p>
               </div>
             </div>
-            <div data-aos="fade-down" className="min-h-72 bg-[#121212] rounded p-3 text-center flex flex-col w-full">
+            <div
+              data-aos="fade-down"
+              className="min-h-72 bg-[#121212] rounded p-3 text-center flex flex-col w-full"
+            >
               <h3 className="text-primary font-semibold mb-2">Daily Salary</h3>
-              <p className="flex-grow flex justify-center items-center text-2xl md:text-5xl">{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(data.user.daily_salary)}</p>
+              <p className="flex-grow flex justify-center items-center text-2xl md:text-5xl">
+                {new Intl.NumberFormat("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                }).format(data.user.daily_salary)}
+              </p>
             </div>
           </div>
           {/* --- */}
-          <div data-aos="fade-up" className="min-h-72 bg-[#121212] rounded p-3 text-center flex flex-col w-full order-3">
-            <h3 className="text-primary font-semibold mb-2">My First Savings</h3>
-            <p className="flex-grow flex justify-center items-center text-2xl md:text-5xl">{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(data.saving.first_saving)}</p>
+          <div
+            data-aos="fade-up"
+            className="min-h-72 bg-[#121212] rounded p-3 text-center flex flex-col w-full order-3"
+          >
+            <h3 className="text-primary font-semibold mb-2">
+              My First Savings
+            </h3>
+            <p className="flex-grow flex justify-center items-center text-2xl md:text-5xl">
+              {new Intl.NumberFormat("id-ID", {
+                style: "currency",
+                currency: "IDR",
+              }).format(data.saving.first_saving)}
+            </p>
           </div>
           {/* --- */}
         </div>
         <div className="relative overflow-x-auto bg-[#121212] rounded">
-          <table data-aos="fade-down" className="w-full text-sm text-left rtl:text-right text-white">
+          <table
+            data-aos="fade-down"
+            className="w-full text-sm text-left rtl:text-right text-white"
+          >
             <thead className="text-xs uppercase bg-primary">
               <tr>
                 <th scope="col" className="px-6 py-3">
@@ -137,43 +198,53 @@ const Trial = () => {
               </tr>
             </thead>
             <tbody>
-              {data.bill.length == 0 &&
+              {data.bill.length == 0 && (
                 <tr>
                   <td className="px-6 py-4 font-medium text-white whitespace-nowrap">
                     There is no history yet
                   </td>
                 </tr>
-              }
-              {typeof window !== "undefined" && !localStorage.getItem("data") &&
-                <tr>
-                  <td className="px-6 py-4 font-medium text-white whitespace-nowrap">
-                    Please fill your data first
-                  </td>
-                </tr>
-              }
-              {typeof window !== "undefined" && localStorage.getItem("data") && data.bill.sort((a, b) => b.ordinal_number - a.ordinal_number).map((item, index) => (
-                <tr key={index} className="border-white">
-                  <th scope="row" className="px-6 py-4">
-                    {index+1}
-                  </th>
-                  <td className="px-6 py-4 font-medium text-white whitespace-nowrap">
-                    {item.is_increase ? '+' : '-'} {new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.total_money_spent)}
-                  </td>
-                  <td className="px-6 py-4">
-                    {item.description}
-                  </td>
-                  <td className="px-6 py-4">
-                    {new Date(item.date_now).toLocaleString('id-ID', { dateStyle: 'medium', timeStyle: 'short' })}
-                  </td>
-                </tr>
-              ))}
+              )}
+              {typeof window !== "undefined" &&
+                !localStorage.getItem("data") && (
+                  <tr>
+                    <td className="px-6 py-4 font-medium text-white whitespace-nowrap">
+                      Please fill your data first
+                    </td>
+                  </tr>
+                )}
+              {typeof window !== "undefined" &&
+                localStorage.getItem("data") &&
+                data.bill
+                  .sort((a, b) => b.ordinal_number - a.ordinal_number)
+                  .map((item, index) => (
+                    <tr key={index} className="border-white">
+                      <th scope="row" className="px-6 py-4">
+                        {index + 1}
+                      </th>
+                      <td className="px-6 py-4 font-medium text-white whitespace-nowrap">
+                        {item.is_increase ? "+" : "-"}{" "}
+                        {new Intl.NumberFormat("id-ID", {
+                          style: "currency",
+                          currency: "IDR",
+                        }).format(item.total_money_spent)}
+                      </td>
+                      <td className="px-6 py-4">{item.description}</td>
+                      <td className="px-6 py-4">
+                        {new Date(item.date_now).toLocaleString("id-ID", {
+                          dateStyle: "medium",
+                          timeStyle: "short",
+                        })}
+                      </td>
+                    </tr>
+                  ))}
             </tbody>
           </table>
         </div>
       </div>
       <ViewFooter />
     </>
-  )
-}
+  );
+};
 
-export default Trial
+export default Trial;
